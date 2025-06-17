5d40752e95e992056618761af8bfa182365c5a68
Hii, I Am Bokade Mehul  🤗 <bokdemehul870@gmail.com>
2025-06-17 12:45:38 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -92,9 +92,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
